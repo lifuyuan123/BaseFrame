cbc6c6aa8b65f7f1ad78f431c47cc64f5cf6a30b
‘lifuyuan <446960231@qq.com>
2022-03-18 17:03:10 +0800

    删除多余ip

--- app/src/main/java/com/example/baseframe/api/Api.kt
+++ app/src/main/java/com/example/baseframe/api/Api.kt
@@ -6,20 +6,5 @@ package com.example.baseframe.api
  * @describe:  存放一些地址
  */
 object Api {
-
-//    const val BASE_URL = "https://gank.io/"
     const val BASE_URL = "https://imgapi.cn/"
-
-    //gsm前端页面
-//    const val GSM_URL = "https://gsmapp.jdjinsui.com:9042/"
-//    const val GSM_URL = "http://172.16.101.111:8081/signin.html?redirect=/tabs/browser"
-//    const val GSM_URL = "http://172.16.102.24:8081"
-//     var GSM_URL = "http://172.16.102.24:8080/"
-//     var GSM_URL = "http://172.16.101.239:8080"
-     var GSM_URL = "http://413g933a53.wicp.vip:8089/"//gsm地图页面
-
-//     var SOCKET_IP = "http://20.20.20.42:9998/"  //内网地址
-//     var SOCKET_IP = "http://413g933a53.wicp.vip:9998/"  //地址
-     var SOCKET_IP = "http://172.16.101.203:9998/"  //陈浩
-//    const val SOCKET_IP = "http://172.16.102.109:9998/"//涂智成
 }
